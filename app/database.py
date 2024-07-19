634c8c88cdbaaf020e7895391f799290a03c2dc7
Ryu_HanSeok <ryuhskyle@gmail.com>
2024-07-19 23:28:16 +0900

    asd

--- app/database.py
+++
@@ -1,6 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from config import settings
-
-client = AsyncIOMotorClient(settings.MONGODB_URL)
-database = client["login_demo"]
-user_collection = database.get_collection("users")
